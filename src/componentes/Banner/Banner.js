08fc22abd3c437db7fdb4b7c12940eb7114d62c5
adilson-psantos <dipersan@gmail.com>
2024-12-02 10:10:06 -0300

    Organizando os arquivos e apagando o que não esta sendo usado no momento do curso

---
+++ src/componentes/Banner/Banner.js
@@ -0,0 +1,12 @@
+import './Banner.css'
+
+function Banner() {
+    //JSX É COMO O REACT LÊ O TRECHO DE CÓDIGO ABAIXO , PARACE HTML MAS NÃO É
+    return(
+        <header className = "banner">
+        <img src="/imagens/banner.png" alt="O Banner principal da página do Organo"/>
+        </header>
+    )
+}
+
+export default Banner
