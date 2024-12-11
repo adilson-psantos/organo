9f93c46cf3e9a94a2ed9dde5d5922364fc744918
adilson-psantos <dipersan@gmail.com>
2024-12-11 09:30:48 -0300

    Criado as const e controlando o estado das variaveis

--- src/componentes/Formulario/index.js
+++ src/componentes/Formulario/index.js
@@ -2,32 +2,68 @@ import CampoTexto from '../CampoTexto'
 import ListaSuspensa from '../ListaSuspensa'
 import './Formulario.css'
 import Botao from '../Botao'
+import { useState } from 'react'
 
 const Formulario = () => {
 
     const times = [
-        'Programação',
-        'Front-End',
-        'Data Science',
-        'Devops',
-        'UX Design',
-        'Mobile',
-        'Inovação e Gestão'
+        'Cientifico',
+        'Cósmico',
+        'Habilidade',
+        'Mistico',
+        'Mutante',        
+        'Tecnologico',        
+        'Superior'
     ]
+
+    const [campeao, setNome] =useState('')
+    const [habilidade, setHabilidade]=useState('')
+    const [imagem, setImagem] = useState('')
+    const [time, setTime] = useState('')
+
+
 
     const aoSalvar = (evento) => {
         evento.preventDefault()
-        console.log('Form foi submetido')
+        console.log('Form foi submetido =>', campeao, habilidade, imagem, time)
     }
     
       return(
         <section className="formulario">
             <form onSubmit={aoSalvar}>
                 <h2>Preencha os dados para criar o card do Colaborador</h2>
-                <CampoTexto obritatorio={true} label="Nome" placeholder="Digite seu nome"/>
-                <CampoTexto obritatorio={true} label="Cargo" placeholder = "Digite seu cargo"/>
-                <CampoTexto label="Imagem" placeholder="Digite o endereço da imagem"/>
-                <ListaSuspensa obritatorio={true} label= "Time" itens={times}/>
+                <CampoTexto 
+                    obritatorio={true} 
+                    label="Campeão" 
+                    placeholder="Digite o nome do Campeão"
+                    valor={campeao}
+                    aoAlterado={valor =>setNome(valor)}
+                />
+
+                <CampoTexto 
+                    obritatorio={true} 
+                    label="Habilidade" 
+                    placeholder = "Digite sua habilidade"
+                    valor={habilidade}
+                    aoAlterado={valor => setHabilidade(valor)}
+
+                />  
+
+                <CampoTexto 
+
+                    label="Imagem" 
+                    placeholder="Digite o endereço da imagem"
+                    valor={imagem}
+                    aoAlterado={valor => setImagem(valor)}
+                />
+                <ListaSuspensa 
+                    obritatorio={true} 
+                    label= "time" 
+                    itens={times}
+                    Valor={time}
+                    aoAlterado={valor => setTime(valor)}
+                                     
+                    />
                 <Botao> 
                     Criar Card
                 </Botao>
