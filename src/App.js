fb6fabacaaccc08a75a612747524075b8b727a88
adilson-psantos <dipersan@gmail.com>
2025-01-08 23:06:58 -0300

    alterando a cor do time

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Time from "./componentes/Time";
 
 function App() {
 
-  const times = [
+  const [times, setTimes] = useState ([
     {
       nome: 'Programação',
       corPrimaria: '#D9F7E9',
@@ -42,7 +42,7 @@ function App() {
       corPrimaria: '#FFEEDF',
       corSecundaria: '#FF8A29'
     },
-  ]
+  ])
 
   const inicial = [
     {
@@ -197,13 +197,23 @@ function App() {
 
   }
 
+  function mudarCorDoTime (cor, nome) {
+    setTimes(times.map(time => {
+      if(time.nome === nome) {
+        time.corSecundaria = cor;
+      }
+      return time;
+    }));      
+  }
+  
   return (
     <div>
       <Banner />
       <Formulario times={times.map(time => time.nome)} aoCadastrar={colaborador => setColaboradores([...colaboradores, colaborador])} />
       <section className="times">
         <h1>Minha organização</h1>
-        {times.map((time, indice) => 
+        {times.map((time, indice) =>
+        mudarCor = {mudarCorDoTime} 
         <Time key={indice} 
         time={time} 
         colaboradores={colaboradores.filter(colaborador => colaborador.time 
