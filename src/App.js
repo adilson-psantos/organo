c331529ab919a9a6e82c05483df84b6627b401aa
adilson-psantos <dipersan@gmail.com>
2025-01-09 09:45:31 -0300

    Alterando a cor secundaria e colocando opacidade

--- src/App.js
+++ src/App.js
@@ -9,38 +9,38 @@ function App() {
   const [times, setTimes] = useState ([
     {
       nome: 'Programação',
-      corPrimaria: '#D9F7E9',
-      corSecundaria: '#57C278'
+      
+      cor: '#57C278'
     },
     {
       nome: 'Front-End',
-      corPrimaria: '#E8F8FF',
-      corSecundaria: '#82CFFA'
+      
+      cor: '#82CFFA'
     },
     {
       nome: 'Data Science',
-      corPrimaria: '#F0F8E2',
-      corSecundaria: '#A6D157'
+      
+      cor: '#A6D157'
     },
     {
       nome: 'Devops',
-      corPrimaria: '#FDE7E8',
-      corSecundaria: '#E06B69'
+      
+      cor: '#E06B69'
     },
     {
       nome: 'UX e Design',
-      corPrimaria: '#FAE9F5',
-      corSecundaria: '#DB6EBF'
+      
+      cor: '#DB6EBF'
     },
     {
       nome: 'Mobile',
-      corPrimaria: '#FFF5D9',
-      corSecundaria: '#FFBA05'
+      
+      cor: '#FFBA05'
     },
     {
       nome: 'Inovação e Gestão',
-      corPrimaria: '#FFEEDF',
-      corSecundaria: '#FF8A29'
+      
+      cor: '#FF8A29'
     },
   ])
 
@@ -200,7 +200,7 @@ function App() {
   function mudarCorDoTime (cor, nome) {
     setTimes(times.map(time => {
       if(time.nome === nome) {
-        time.corSecundaria = cor;
+        time.cor = cor;
       }
       return time;
     }));      
@@ -213,8 +213,8 @@ function App() {
       <section className="times">
         <h1>Minha organização</h1>
         {times.map((time, indice) =>
-        mudarCor = {mudarCorDoTime} 
-        <Time key={indice} 
+        <Time mudarCor = {mudarCorDoTime} 
+        key={indice} 
         time={time} 
         colaboradores={colaboradores.filter(colaborador => colaborador.time 
         === time.nome)} 
