ccd87a3943fa7edf7f4a3d17c7303123d0b7db4f
adilson-psantos <dipersan@gmail.com>
2025-01-12 09:22:26 -0300

    Criando ids

--- src/componentes/Time/index.js
+++ src/componentes/Time/index.js
@@ -5,8 +5,8 @@ import hexToRgba from 'hex-to-rgba';
 const Time = ({ time, colaboradores, aoDeletar, mudarCor }) => {
     return (
         colaboradores.length > 0 && <section className='time' style={{ backgroundImage: 'url(/imagens/fundo.png)', backgroundColor: hexToRgba(time.cor, '0.6') }}>
-            <input onChange={evento => mudarCor(evento.target.value,time.nome)} value={cor} type='color' className='input-cor'/>
-            <h3 style={{ borderColor: time.cor }}>{time.nome}</h3>
+            <input onChange={evento => mudarCor(evento.target.value,time.id)} value={cor} type='color' className='input-cor'/>
+            <h3 style={{ borderColor: time.cor }}>{time.id}</h3>
             <div className='colaboradores'>
                 {colaboradores.map((colaborador, indice) => {
                     
